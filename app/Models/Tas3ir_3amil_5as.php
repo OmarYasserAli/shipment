796b76097fe63672d45d8b3eec14563a3b67ce2b
ziad abo alkamar <ziad-lovelyboy@hotmail.com>
2022-06-28 15:35:01 +0300

    third time

--- app/Models/Tas3ir_3amil_5as.php
+++ app/Models/Tas3ir_3amil_5as.php
@@ -8,10 +8,10 @@ class Tas3ir_3amil_5as extends Model
 {
 	use \Awobaz\Compoships\Compoships;
 	public $timestamps = false;
-	
+
     protected $primaryKey = 'code_';
     protected $table = 'special_prices_tb';
     protected $guarded = [];
 
-    
+
 }
