1fd7b2d9bef4b61eda68efe9ca3087bddc1c6929
ziad abo alkamar <ziad-lovelyboy@hotmail.com>
2022-07-18 16:59:31 +0300

    ziad

--- resources/views/layout/partial/nav.blade.php
+++ resources/views/layout/partial/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="side-nav">
     <a href="" class="intro-x flex items-center pl-5 pt-4">
 
-        <img alt="Midone - HTML Admin Template" class="w-10" src="{{asset('assets/'.\App\Models\CompanyInfo::where('branch_',Auth::user()->Auth::user()->branch)->first()->image_data)}}">
+        <img alt="Midone - HTML Admin Template" class="w-10" src="{{asset('assets/'.\App\Models\CompanyInfo::where('branch_',Auth::user()->branch)->first()->image_data)}}">
         <span class="hidden xl:block text-white text-lg ml-3 mr-3"> {{\App\Models\CompanyInfo::where('branch_',Auth::user()->branch)->first()->name_}} </span>
     </a>
     <div class="side-nav__devider my-6"></div>
