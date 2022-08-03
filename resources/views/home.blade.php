b15facf7af6fcefa8ecf4e0969f038757db66883
omarYasserAli <3omaryasser74@gmail.com>
2022-08-03 09:10:18 +0200

    social

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 <div class="content">
     <!-- BEGIN: Top Bar -->
     @include('layout.partial.topbar')
-    {{-- <div class="grid grid-cols-12 gap-6">
+    <div class="grid grid-cols-12 gap-6">
         <div class="col-span-12 2xl:col-span-9">
             <div class="grid grid-cols-12 gap-6">
                 <!-- BEGIN: General Report -->
@@ -124,7 +124,7 @@
             </div>
         </div>
         
-    </div> --}}
+    </div>
 </div>
   
 
